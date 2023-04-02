2fe81bef65893491264118d8162985d038e2b21f
sharkzhs83 <sharkzhs83@naver.com>
2023-04-02 22:04:58 +0900

    엔더맨, 음식 억까 추가

--- src/main/kotlin/io/github/sharkzhs83/hardcore/events/Events.kt
+++ src/main/kotlin/io/github/sharkzhs83/hardcore/events/Events.kt
@@ -1,6 +1,5 @@
 package io.github.sharkzhs83.hardcore.events
 
-import io.github.sharkzhs83.hardcore.HARDCORE
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.Sound
@@ -12,8 +11,8 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityExplodeEvent
 import org.bukkit.event.entity.EntityShootBowEvent
 import org.bukkit.event.inventory.CraftItemEvent
+import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.inventory.ItemStack
-import org.bukkit.metadata.FixedMetadataValue
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
@@ -58,35 +57,36 @@ class Events : Listener {
         val range = 1..5
         val num = range.random()
 
-
-        if(num == 1) {
-            if(block.type == Material.IRON_ORE) {
+        if (num == 1) {
+            if (block.type == Material.IRON_ORE || block.type == Material.DEEPSLATE_IRON_ORE) {
+                event.isDropItems = false
                 val newItem = ItemStack(Material.COAL, 2)
-
+                player.sendMessage("${ChatColor.BLACK}철인줄 알았는데 석탄이었다..")
                 block.world.dropItemNaturally(block.location, newItem)
             }
-            else if(block.type == Material.GOLD_ORE) {
+            else if (block.type == Material.GOLD_ORE || block.type == Material.DEEPSLATE_GOLD_ORE) {
+                event.isDropItems = false
                 val newItem = ItemStack(Material.RAW_IRON, 2)
-
+                player.sendMessage("${ChatColor.GREEN}금인줄 알았는데 철이었다..")
                 block.world.dropItemNaturally(block.location, newItem)
             }
-            else if(block.type == Material.LAPIS_ORE) {
+            else if (block.type == Material.LAPIS_ORE || block.type == Material.DEEPSLATE_LAPIS_ORE) {
+                event.isDropItems = false
                 val newItem = ItemStack(Material.ICE, 2)
-
+                player.sendMessage("${ChatColor.AQUA}청금석인줄 알았는데 얼음이었다..")
                 block.world.dropItemNaturally(block.location, newItem)
             }
-            else if(block.type == Material.DIAMOND_ORE) {
+            else if (block.type == Material.DIAMOND_ORE || block.type == Material.DEEPSLATE_DIAMOND_ORE) {
+                event.isDropItems = false
                 val newItem = ItemStack(Material.LAPIS_LAZULI, 2)
-
+                player.sendMessage("${ChatColor.BLUE}다이아몬드인줄 알았는데 청금석이었다..")
                 block.world.dropItemNaturally(block.location, newItem)
             }
-        }
-
-
+            }
     }
 
 
-    //부활
+    //크리퍼 부활
     @EventHandler
     fun onExplode(event: EntityExplodeEvent) {
         val entity = event.entity
@@ -161,6 +161,37 @@ class Events : Listener {
 
         }
     }
+
+    //엔더맨 억까
+    @EventHandler
+    fun onPlayerDamageByEnderMan(event: EntityDamageByEntityEvent) {
+        if (event.damager.type == EntityType.ENDERMAN && event.entity is Player) {
+            val player = event.entity as Player
+            val entity = event.damager as LivingEntity
+
+            player.teleport(player.location.add(0.0, 9.0, 0.0))
+            player.playSound(player.location, Sound.ENTITY_ENDERMAN_TELEPORT, 0.7f, 1.0f)
+
+        }
+    }
+
+
+    @EventHandler
+    fun onPlayerEatFood(event: PlayerItemConsumeEvent) {
+        val player = event.player as Player
+        val item = event.item
+
+        val range = 1..20
+        val num = range.random()
+
+        if (item.type.isEdible) {
+            if (num == 1) {
+                player.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS, 60, 5,false, false))
+                player.sendMessage("${ChatColor.RED}배탈이 나서 눈 앞이 깜깜해졌다!")
+            }
+        }
+    }
+
 
     //아이템 억까
     @EventHandler
