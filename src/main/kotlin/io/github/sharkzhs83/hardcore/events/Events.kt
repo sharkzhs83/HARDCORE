45b53d5ea9bd26bf582614a9f14acb3876ed98d5
sharkzhs83 <sharkzhs83@naver.com>
2023-04-02 18:48:58 +0900

    아이템, 광물, 거미 억까 추가

--- src/main/kotlin/io/github/sharkzhs83/hardcore/events/Events.kt
+++ src/main/kotlin/io/github/sharkzhs83/hardcore/events/Events.kt
@@ -3,6 +3,7 @@ package io.github.sharkzhs83.hardcore.events
 import io.github.sharkzhs83.hardcore.HARDCORE
 import org.bukkit.ChatColor
 import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -10,8 +11,11 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityExplodeEvent
 import org.bukkit.event.entity.EntityShootBowEvent
+import org.bukkit.event.inventory.CraftItemEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.metadata.FixedMetadataValue
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 
 class Events : Listener {
 
@@ -41,12 +45,48 @@ class Events : Listener {
                 player.sendMessage("${ChatColor.GREEN}크리퍼가 나타났습니다!")
             }
         }
-        else
-            return
 
 
     }
 
+    //광물 억까
+    @EventHandler
+    fun onBlockBreakOre(event: BlockBreakEvent) {
+        val player = event.player
+        val block = event.block
+
+        val range = 1..5
+        val num = range.random()
+
+
+        if(num == 1) {
+            if(block.type == Material.IRON_ORE) {
+                val newItem = ItemStack(Material.COAL, 2)
+
+                block.world.dropItemNaturally(block.location, newItem)
+            }
+            else if(block.type == Material.GOLD_ORE) {
+                val newItem = ItemStack(Material.RAW_IRON, 2)
+
+                block.world.dropItemNaturally(block.location, newItem)
+            }
+            else if(block.type == Material.LAPIS_ORE) {
+                val newItem = ItemStack(Material.ICE, 2)
+
+                block.world.dropItemNaturally(block.location, newItem)
+            }
+            else if(block.type == Material.DIAMOND_ORE) {
+                val newItem = ItemStack(Material.LAPIS_LAZULI, 2)
+
+                block.world.dropItemNaturally(block.location, newItem)
+            }
+        }
+
+
+    }
+
+
+    //부활
     @EventHandler
     fun onExplode(event: EntityExplodeEvent) {
         val entity = event.entity
@@ -106,5 +146,44 @@ class Events : Listener {
             }
         }
     }
+
+    //거미 억까
+    @EventHandler
+    fun onPlayerDamageBySpider(event: EntityDamageByEntityEvent) {
+        if (event.damager.type == EntityType.SPIDER && event.entity is Player) {
+            val player = event.entity as Player
+            val entity = event.damager as LivingEntity
+
+            player.addPotionEffect(PotionEffect(PotionEffectType.SLOW, 100, 5,false, false))
+            entity.addPotionEffect(PotionEffect(PotionEffectType.INVISIBILITY, 100,1))
+
+
+
+        }
+    }
+
+    //아이템 억까
+    @EventHandler
+    fun onMake(event: CraftItemEvent) {
+        val player = event.whoClicked
+        val range = 1..5
+        val num = range.random()
+
+        if(num == 1) {
+            event.currentItem = ItemStack(Material.AIR)
+            if (player is Player) {
+                for (ingredient in event.inventory.matrix) {
+                    if (ingredient != null && ingredient.type != Material.AIR) {
+                        ingredient.amount = ingredient.amount - 1
+                        player.sendMessage("${ChatColor.RED}제작에 실패하였습니다!")
+                        player.playSound(player.location, Sound.BLOCK_ANVIL_LAND, 0.5f, 1.0f)
+                    }
+                }
+            }
+        }
+
+
+    }
+
 }
 
